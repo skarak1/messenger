7d195c665b81e83a5d31c39e5c6f0875959bd8c2
skarak1 <sudipkarak@gmail.com>
2025-11-01 00:02:56 +0530

    user register backend 1

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT|| 5000;
+const dotenv = require('dotenv')
 
-//data pass
-app.get('/', (req, res) => {
-    res.send('This is from backend server!');
-});
+const databaseConnect = require('./config/database')
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+dotenv.config({
+     path : 'backend/config/config.env'
+})
+
+const PORT = process.env.PORT || 5000
+app.get('/', (req, res)=>{
+     res.send('This is from backend Sever')
+})
+
+databaseConnect();
+
+app.listen(PORT, ()=>{
+     console.log(`Server is running on port ${PORT}`)
+})
