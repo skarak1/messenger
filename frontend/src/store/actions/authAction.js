4eb0409d74703e92b69e1bc47035de21a63fcfcb
skarak1 <sudipkarak@gmail.com>
2025-11-07 14:54:59 +0530

    user information store in redux 1

--- frontend/src/store/actions/authAction.js
+++ frontend/src/store/actions/authAction.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {REGISTER_FAIL} from "../types/authType";
 
 export const userRegister = (data) => {
      return async (dispatch) => {
@@ -14,6 +15,12 @@ export const userRegister = (data) => {
 
           } catch(error){
                console.log(error.response.data);
+               dispatch({
+                    type: REGISTER_FAIL,
+                    payload:{
+                         error : error.response.data.error.errorMessage 
+                    }
+                })
           }
 
      }
